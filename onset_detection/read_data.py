bc4c80992c34f08899ff8ad252cf683b33c6baae
Michel Pluess <michel.pluess@students.fhnw.ch>
2017-03-16 16:48:16 +0100

    Custom onset metric like in the Fraunhofer paper (50ms / 5 frames tolerance after onset). Visualize 1D convolution filters.

--- onset_detection/read_data.py
+++ onset_detection/read_data.py
@@ -31,6 +31,7 @@ def read_y_xml(path_to_xml, length_seconds, dataset, frame_rate_hz=100):
     tree = ElementTree.parse(path_to_xml)
     root = tree.getroot()
     y = _init_y(length_seconds, frame_rate_hz)
+    y_start_only = _init_y(length_seconds, frame_rate_hz)
     for root_child in root:
         if root_child.tag == 'transcription':
             for event in root_child:
@@ -40,24 +41,23 @@ def read_y_xml(path_to_xml, length_seconds, dataset, frame_rate_hz=100):
                     if event_child.tag == 'onsetSec':
                         onset_time = float(event_child.text)
                         index = _onset_index(onset_time, frame_rate_hz)
-                        _set_onset_label(y, index, dataset)
+                        _set_onset_label(y, y_start_only, index, dataset)
             break
 
-    y = np.reshape(y, (-1, 1))
-    return y
+    return y, y_start_only
 
 
 def read_y_csv(path_to_csv, length_seconds, dataset, frame_rate_hz=100):
     y = _init_y(length_seconds, frame_rate_hz)
+    y_start_only = _init_y(length_seconds, frame_rate_hz)
     with open(path_to_csv) as f:
         for line in f:
             line_split = line.rstrip().split(',')
             onset_time = float(line_split[0])
             index = _onset_index(onset_time, frame_rate_hz)
-            _set_onset_label(y, index, dataset)
+            _set_onset_label(y, y_start_only, index, dataset)
 
-    y = np.reshape(y, (-1, 1))
-    return y
+    return y, y_start_only
 
 
 def _init_y(length_seconds, frame_rate_hz):
@@ -68,20 +68,20 @@ def _onset_index(onset_time, frame_rate_hz):
     return int(onset_time * frame_rate_hz)
 
 
-def _set_onset_label(y, index, dataset):
+def _set_onset_label(y, y_start_only, index, dataset):
     start = index
     end = index
-    if dataset == 'ds1':
+    if dataset == 1:
         # Python-style indices: start included, end not included
         start += -2
         end += 1
-    elif dataset == 'ds2':
+    elif dataset == 2:
         start += 2
         end += 5
-    elif dataset == 'ds3':
+    elif dataset == 3:
         start += 0
         end += 3
-    elif dataset == 'ds4':
+    elif dataset == 4:
         start += 0
         end += 2
     else:
@@ -90,52 +90,52 @@ def _set_onset_label(y, index, dataset):
     start = max(0, start)
     end = min(len(y), end)
     if end - start > 0:
-        y[start:end] = 1
-
         # y[index] = 1
+        y[start:end] = 1
+        y_start_only[start] = 1
 
 
 def read_X_y(path_to_wav, path_to_truth, dataset, truth_format):
     X_part, length_seconds = read_X(path_to_wav)
     if X_part is not None:
         if truth_format == 'xml':
-            y_part = read_y_xml(path_to_truth, length_seconds, dataset)
+            y_part, y_start_only_part = read_y_xml(path_to_truth, length_seconds, dataset)
         elif truth_format == 'csv':
-            y_part = read_y_csv(path_to_truth, length_seconds, dataset)
+            y_part, y_start_only_part = read_y_csv(path_to_truth, length_seconds, dataset)
         else:
             raise ValueError('Unknown truth format')
 
         if X_part.shape[0] != y_part.shape[0]:
             raise ValueError(
                 'X_part vs. y_part shape mismatch: ' + str(X_part.shape[0]) + ' != ' + str(y_part.shape[0]))
-        return X_part, y_part
+        return X_part, y_part, y_start_only_part
     else:
-        return None, None
+        return None, None, None
 
 
 def read_data(active_datasets):
     # TODO dataset einchecken, damit aenderungen zentral gemacht werden (anderes, privates repo)
     dir_tuples = []
-    if 'ds1' in active_datasets:
+    if 1 in active_datasets:
         path_to_ds_1 = r'data\IDMT-SMT-GUITAR_V2\dataset1'
         for guitar_desc in listdir(path_to_ds_1):
             dir_tuples.append((
                 os.path.join(path_to_ds_1, guitar_desc, 'audio'),
                 os.path.join(path_to_ds_1, guitar_desc, 'annotation'),
-                'ds1',
+                1,
             ))
 
-    if 'ds2' in active_datasets:
+    if 2 in active_datasets:
         dir_tuples.append((
             r'data\IDMT-SMT-GUITAR_V2\dataset2\audio',
             r'data\IDMT-SMT-GUITAR_V2\dataset2\annotation',
-            'ds2',
+            2,
         ))
-    if 'ds3' in active_datasets:
+    if 3 in active_datasets:
         dir_tuples.append((
             r'data\IDMT-SMT-GUITAR_V2\dataset3\audio',
             r'data\IDMT-SMT-GUITAR_V2\dataset3\annotation',
-            'ds3',
+            3,
         ))
 
     file_tuples = []
@@ -151,7 +151,7 @@ def read_data(active_datasets):
             else:
                 warn('Skipping non-wav file ' + path_to_wav)
 
-    if 'ds4' in active_datasets:
+    if 4 in active_datasets:
         for path_to_ds in [r'data\IDMT-SMT-GUITAR_V2\dataset4\Career SG', r'data\IDMT-SMT-GUITAR_V2\dataset4\Ibanez 2820']:
             for tempo in listdir(path_to_ds):
                 path_to_tempo = os.path.join(path_to_ds, tempo)
@@ -165,7 +165,7 @@ def read_data(active_datasets):
                             if isdir(path_to_onsets):
                                 path_to_csv = os.path.join(path_to_onsets, wav_file.replace('.wav', '.csv'))
                                 if isfile(path_to_csv):
-                                    file_tuples.append((path_to_wav, path_to_csv, 'ds4', 'csv'))
+                                    file_tuples.append((path_to_wav, path_to_csv, 4, 'csv'))
                                 else:
                                     # TODO fallback to other formats
                                     warn('Skipping ' + path_to_wav + ': no truth csv')
@@ -174,16 +174,16 @@ def read_data(active_datasets):
                         else:
                             warn('Skipping non-wav file ' + path_to_wav)
 
-    X = []
-    y = []
+    X_parts = []
+    y_parts = []
+    y_start_only_parts = []
+    ds_labels = []
     for path_to_wav, path_to_truth, dataset, truth_format in file_tuples:
-        X_part, y_part = read_X_y(path_to_wav, path_to_truth, dataset, truth_format)
-        if X_part is not None and y_part is not None:
-            X.append(X_part)
-            y.append(y_part)
+        X_part, y_part, y_start_only_part = read_X_y(path_to_wav, path_to_truth, dataset, truth_format)
+        if X_part is not None and y_part is not None and y_start_only_part is not None:
+            X_parts.append(X_part)
+            y_parts.append(y_part)
+            y_start_only_parts.append(y_start_only_part)
+            ds_labels.append(dataset)
 
-    X = np.concatenate(X)
-    y = np.concatenate(y)
-    y = y.ravel()
-
-    return X, y
+    return X_parts, y_parts, y_start_only_parts, ds_labels
