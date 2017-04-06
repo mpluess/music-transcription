5fb0e617e4bb5cd48b17806789c5410f6151c745
Michel Pluess <michel.pluess@students.fhnw.ch>
2017-04-06 18:08:26 +0200

    Labels changed: only label one frame that includes the originally labelled onset time. Metric changed: count 2 frames before and after as correct.

--- onset_detection/metrics.py
+++ onset_detection/metrics.py
@@ -1,20 +1,27 @@
 import numpy as np
 
 
-def onset_metric(y, y_start_only, y_predicted, frame_tol=5):
-    """Assumes y is grouped by wav file, sorted by time asc."""
+def onset_metric(y, y_actual_onset_only, y_predicted, n_tolerance_frames_plus_minus):
+    """Assumes y is grouped by wav file, sorted by time asc.
 
-    onset_start_indices = np.where(y_start_only == 1)[0]
-    # Make sure all onset start indices are indeed labelled as onsets in the actual ground truth y. Otherwise something is off.
-    assert len(onset_start_indices) == np.sum(y[onset_start_indices] == 1)
+    n_tolerance_frames_plus_minus: n_tolerance_frames_plus_minus before and n_tolerance_frames_plus_minus after
+    the frame labelled as onset will also count as true positive.
+    """
+
+    actual_onset_indices = np.where(y_actual_onset_only == 1)[0]
+    # Make sure all actual onset indices are indeed labelled as onsets in the ground truth y used for training.
+    # Otherwise something is off.
+    assert len(actual_onset_indices) == np.sum(y[actual_onset_indices] == 1)
 
     tp = 0
     fn = 0
-    onset_indices = set()
-    for start_index in onset_start_indices:
+    tolerated_onset_indices = set()
+    for actual_onset_index in actual_onset_indices:
         is_tp = False
-        for i in range(start_index, start_index + frame_tol):
-            onset_indices.add(i)
+        start = max(0, actual_onset_index - n_tolerance_frames_plus_minus)
+        end = min(len(y), actual_onset_index + n_tolerance_frames_plus_minus + 1)
+        for i in range(start, end):
+            tolerated_onset_indices.add(i)
             # If any of the labels equals 1 in the region of frame_tol frames: mark as true positive.
             if y_predicted[i] == 1:
                 is_tp = True
@@ -24,9 +31,9 @@ def onset_metric(y, y_start_only, y_predicted, frame_tol=5):
             fn += 1
 
     # Create index mask which is the inverse of the tolerated onset indices to detect false positives.
-    onset_indices_array = np.array(list(onset_indices), dtype=np.int32)
+    tolerated_onset_indices_array = np.array(list(tolerated_onset_indices), dtype=np.int32)
     fp_mask = np.ones(len(y), np.bool)
-    fp_mask[onset_indices_array] = 0
+    fp_mask[tolerated_onset_indices_array] = 0
     fp = np.sum(y_predicted[fp_mask] == 1)
 
     precision = tp / (tp + fp)
--- onset_detection/read_data.py
+++ onset_detection/read_data.py
@@ -7,12 +7,17 @@ from xml.etree import ElementTree
 from warnings import warn
 
 
-def read_X(path_to_wav, subsampling_step, frame_rate_hz=100):
+def read_X(path_to_wav, frame_rate_hz, expected_sample_rate, subsampling_step):
     # scipy.io.wavfile is not able to read 24-bit data hence the need to use this alternative library
     samples, sample_rate = soundfile.read(path_to_wav)
     if len(samples.shape) > 1:
-        warn('Cannot handle stereo signal (' + path_to_wav + '), skipping file.')
+        warn('Skipping ' + path_to_wav + ', cannot handle stereo signal.')
         return None, -1
+    elif sample_rate != expected_sample_rate:
+        warn('Skipping ' + path_to_wav +
+             ', sample rate ' + str(sample_rate) + ' != expected sample rate ' + str(expected_sample_rate) + '.')
+        return None, -1
+
     if sample_rate % frame_rate_hz != 0:
         raise ValueError('Sample rate ' + str(sample_rate) + ' % frame rate ' + str(frame_rate_hz) + ' != 0')
     samples_per_frame = int(sample_rate / frame_rate_hz)
@@ -31,7 +36,7 @@ def read_y_xml(path_to_xml, length_seconds, dataset, frame_rate_hz=100):
     tree = ElementTree.parse(path_to_xml)
     root = tree.getroot()
     y = _init_y(length_seconds, frame_rate_hz)
-    y_start_only = _init_y(length_seconds, frame_rate_hz)
+    y_actual_onset_only = _init_y(length_seconds, frame_rate_hz)
     for root_child in root:
         if root_child.tag == 'transcription':
             for event in root_child:
@@ -41,23 +46,23 @@ def read_y_xml(path_to_xml, length_seconds, dataset, frame_rate_hz=100):
                     if event_child.tag == 'onsetSec':
                         onset_time = float(event_child.text)
                         index = _onset_index(onset_time, frame_rate_hz)
-                        _set_onset_label(y, y_start_only, index, dataset)
+                        _set_onset_label_orig(y, y_actual_onset_only, index)
             break
 
-    return y, y_start_only
+    return y, y_actual_onset_only
 
 
 def read_y_csv(path_to_csv, length_seconds, dataset, frame_rate_hz=100):
     y = _init_y(length_seconds, frame_rate_hz)
-    y_start_only = _init_y(length_seconds, frame_rate_hz)
+    y_actual_onset_only = _init_y(length_seconds, frame_rate_hz)
     with open(path_to_csv) as f:
         for line in f:
             line_split = line.rstrip().split(',')
             onset_time = float(line_split[0])
             index = _onset_index(onset_time, frame_rate_hz)
-            _set_onset_label(y, y_start_only, index, dataset)
+            _set_onset_label_orig(y, y_actual_onset_only, index)
 
-    return y, y_start_only
+    return y, y_actual_onset_only
 
 
 def _init_y(length_seconds, frame_rate_hz):
@@ -68,51 +73,65 @@ def _onset_index(onset_time, frame_rate_hz):
     return int(onset_time * frame_rate_hz)
 
 
-def _set_onset_label(y, y_start_only, index, dataset):
-    start = index
-    end = index
-    if dataset == 1:
-        # Python-style indices: start included, end not included
-        start += -2
-        end += 1
-    elif dataset == 2:
-        start += 2
-        end += 5
-    elif dataset == 3:
-        start += 0
-        end += 3
-    elif dataset == 4:
-        start += 0
-        end += 2
-    else:
-        raise ValueError('Invalid dataset label')
-
-    start = max(0, start)
-    end = min(len(y), end)
-    if end - start > 0:
-        # y[index] = 1
-        y[start:end] = 1
-        y_start_only[start] = 1
+def _set_onset_label_orig(y, y_actual_onset_only, index):
+    y[index] = 1
+    y_actual_onset_only[index] = 1
 
 
-def read_X_y(path_to_wav, path_to_truth, dataset, truth_format, subsampling_step):
-    X_part, length_seconds = read_X(path_to_wav, subsampling_step)
+def _set_onset_label_orig_with_neighbors(y, y_actual_onset_only, index):
+    # index = 5: start = 4, end = 7, 4:7 = 1 -> 4, 5, 6 = 1
+    start = max(0, index - 1)
+    end = min(len(y), index + 2)
+    y[start:end] = 1
+    y_actual_onset_only[index] = 1
+
+
+# def _set_onset_label_adjusted_range(y, y_start_only, index, dataset):
+#     start = index
+#     end = index
+#     if dataset == 1:
+#         # Python-style indices: start included, end not included
+#         start += -2
+#         end += 1
+#     elif dataset == 2:
+#         start += 2
+#         end += 5
+#     elif dataset == 3:
+#         start += 0
+#         end += 3
+#     elif dataset == 4:
+#         start += 0
+#         end += 2
+#     else:
+#         raise ValueError('Invalid dataset label')
+#
+#     start = max(0, start)
+#     end = min(len(y), end)
+#     if end - start > 0:
+#         # y[index] = 1
+#         y[start:end] = 1
+#         y_start_only[start] = 1
+
+
+def read_X_y(path_to_wav, frame_rate_hz, expected_sample_rate, subsampling_step,
+             path_to_truth, truth_format, dataset):
+    X_part, length_seconds = read_X(path_to_wav, frame_rate_hz, expected_sample_rate, subsampling_step)
     if X_part is not None:
         if truth_format == 'xml':
-            y_part, y_start_only_part = read_y_xml(path_to_truth, length_seconds, dataset)
+            y_part, y_actual_onset_only_part = read_y_xml(path_to_truth, length_seconds, dataset)
         elif truth_format == 'csv':
-            y_part, y_start_only_part = read_y_csv(path_to_truth, length_seconds, dataset)
+            y_part, y_actual_onset_only_part = read_y_csv(path_to_truth, length_seconds, dataset)
         else:
             raise ValueError('Unknown truth format')
 
         if X_part.shape[0] != y_part.shape[0]:
             raise ValueError('X_part vs. y_part shape mismatch: ' + str(X_part.shape[0]) + ' != ' + str(y_part.shape[0]))
-        return X_part, y_part, y_start_only_part
+        return X_part, y_part, y_actual_onset_only_part
     else:
         return None, None, None
 
 
-def read_data(active_datasets, subsampling_step=4):
+def read_data(active_datasets, frame_rate_hz, expected_sample_rate, subsampling_step):
     # TODO dataset einchecken, damit aenderungen zentral gemacht werden (anderes, privates repo)
     dir_tuples = []
     if 1 in active_datasets:
@@ -146,9 +165,9 @@ def read_data(active_datasets, subsampling_step=4):
                 if isfile(path_to_xml):
                     file_tuples.append((path_to_wav, path_to_xml, ds, 'xml'))
                 else:
-                    warn('No truth found for ' + wav_file + ', skipping file.')
+                    warn('Skipping ' + wav_file + ', no truth found.')
             else:
-                warn('Skipping non-wav file ' + path_to_wav)
+                warn('Skipping ' + path_to_wav + ', not a .wav file.')
 
     if 4 in active_datasets:
         for path_to_ds in [r'data\IDMT-SMT-GUITAR_V2\dataset4\Career SG', r'data\IDMT-SMT-GUITAR_V2\dataset4\Ibanez 2820']:
@@ -167,22 +186,23 @@ def read_data(active_datasets, subsampling_step=4):
                                     file_tuples.append((path_to_wav, path_to_csv, 4, 'csv'))
                                 else:
                                     # TODO fallback to other formats
-                                    warn('Skipping ' + path_to_wav + ': no truth csv')
+                                    warn('Skipping ' + path_to_wav + ', no truth csv found.')
                             else:
-                                warn('Skipping ' + path_to_wav + ': no onset folder')
+                                warn('Skipping ' + path_to_wav + ', no onset folder.')
                         else:
-                            warn('Skipping non-wav file ' + path_to_wav)
+                            warn('Skipping ' + path_to_wav + ', not a .wav file.')
 
     X_parts = []
     y_parts = []
-    y_start_only_parts = []
+    y_actual_onset_only_parts = []
     ds_labels = []
     for path_to_wav, path_to_truth, dataset, truth_format in file_tuples:
-        X_part, y_part, y_start_only_part = read_X_y(path_to_wav, path_to_truth, dataset, truth_format, subsampling_step)
-        if X_part is not None and y_part is not None and y_start_only_part is not None:
+        X_part, y_part, y_actual_onset_only_part = read_X_y(path_to_wav, frame_rate_hz, expected_sample_rate,
+                                                            subsampling_step, path_to_truth, truth_format, dataset)
+        if X_part is not None and y_part is not None and y_actual_onset_only_part is not None:
             X_parts.append(X_part)
             y_parts.append(y_part)
-            y_start_only_parts.append(y_start_only_part)
+            y_actual_onset_only_parts.append(y_actual_onset_only_part)
             ds_labels.append(dataset)
 
-    return X_parts, y_parts, y_start_only_parts, ds_labels
+    return X_parts, y_parts, y_actual_onset_only_parts, ds_labels
